6473a299a09242d1bc844309b5a6353afae2c8b1
MantasPetrosiusI <116674326+MantasPetrosiusI@users.noreply.github.com>
2023-01-18 22:57:10 +0000

    duringBrief

--- index.js
+++ index.js
@@ -14,16 +14,16 @@ renderBooks = (books) => {
   console.log(row.innerHTML);
   books.forEach((book) => {
     row.innerHTML += `
-    <div class="col-12 col-sm-6 col-4 col-lg-2 m-4 border border-1>
-        <div class="card>
+    <div class="col col-sm-6 col-md-4 col-lg-3 m-4 border border-1">
+        <div class="card">
             <img src="${book.img}" class="card-img-top mt-2" alt="...">
             <div class="card-body">
                 <h3 class="card-title">${book.title}</h3>
                 <h4 class="card-category">${book.category}</h4>
                 <h6 class="card-price">${book.price}</h6>
 
-                <button type="button" class="btn btn-primary" onClick="addToCart">Add to Cart</button>
-                <button type="button" class="btn btn-danger" onClick="addToCart">Skip</button>
+                <button type="button" class="btn btn-primary" onClick="addToCart(this)">Add to Cart</button>
+                <button type="button" class="btn btn-danger" onClick="skip()">Skip</button>
             </div>
         </div>
     </div>
@@ -31,19 +31,20 @@ renderBooks = (books) => {
   });
 };
 
-const addToCart = (btn) => {
-  const cart = document.querySelector(".ul");
-  btn.closest(".card").classList.add("border-sucess");
+const addToCart = (button) => {
+  let booksInCart = document.querySelector("ul");
+  let selectedBook = button.closest(".card").innerHTML;
+  booksInCart.innerHTML += `<li>${selectedBook}</li>`;
 };
-window.onload = getBooks();
+
+function skip() {}
+
+const skipBook = (window.onload = getBooks());
 /*///////////////////////////////////////////////////////////////////////
 An input field to filter the books
  
 
 Tasks you need to perform to achieve those requirements:
-
-Display all the books using template literals and .forEach() or .map()
-
 
 Add an add to cart button into each item
 When this button is pressed: 1) add the item to another list (the cart), and 2) change the card styling to show that the element is in the cart (eg. red border, a badge, an icon… you choose)
